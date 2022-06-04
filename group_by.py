2912bf0f55965e16fac54e7a96591a0c2f316dcb
dds7ae <dds7ae@virginia.edu>
2022-06-04 10:21:01 -0700

    Working changes

--- group_by.py
+++ group_by.py
@@ -3,7 +3,7 @@ import pandas as pd  # Data Wrangling
 
 df = pd.read_csv("pga_tour_data.csv")
 df = df.iloc[:, 1:]
-df1 = df.groupby('NAME')[['ROUNDS', 'DRIVE_DISTANCE', 'PAR OR BETTER', 'MISSED GIR', 'US_OPEN_TOP_10', 'POINTS',
+df1 = df.groupby('NAME')[['ROUNDS', 'DRIVE_DISTANCE', 'PAR OR BETTER', 'MISSED GIR', 'US_OPEN_TOP_10',
                          'TOP 10', '1ST']].sum()
 print(df1)
 df2 = df.groupby(['NAME'])[['SCORING', 'FWY_%', 'GIR_%', 'SG_P', 'SG_TTG', 'SG_T', 'SCRAMBLING_P']].agg(tuple).\
